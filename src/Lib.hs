ef2d0b6b3af29f48fb088078677508b6096bf1b8
Claudio Bley <claudio.bley@gmail.com>
2019-07-29 11:20:20 +0200

    Restructure project

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,13 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (getOutput) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           System.Exit            (ExitCode (..))
+import           System.Process
+
+getOutput :: String -> IO String
+getOutput input = do
+  let cmd = "/home/claudio/.nix-profile/bin/jq"
+  let args = ["--color-output", input, "/home/claudio/Downloads/RBMN_API.json"]
+  (exitc, stdout, err) <- readProcessWithExitCode cmd args ""
+  case exitc of
+    ExitFailure _ -> error err
+    ExitSuccess   -> pure stdout
---
+++ src/UI.hs
@@ -0,0 +1,99 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+module UI (run) where
+
+import           Control.Monad          (void)
+import           Control.Monad.IO.Class (liftIO)
+import           Data.Monoid            ((<>))
+import qualified Graphics.Vty           as V
+
+import qualified Data.Text              as DT
+
+import           Lens.Micro             ((%~), (&), (.~), (^.))
+import           Lens.Micro.TH
+
+import           Brick.AttrMap          (attrMap)
+import           Brick.Forms            (Form, allFieldsValid, checkboxField,
+                                         editPasswordField, editShowableField,
+                                         editTextField, focusedFormInputAttr,
+                                         formFocus, formState, handleFormEvent,
+                                         invalidFields, invalidFormInputAttr,
+                                         newForm, radioField, renderForm,
+                                         setFieldValid, (@@=))
+import qualified Brick.Main             as M
+import           Brick.Types            (ViewportType (Both, Horizontal, Vertical),
+                                         Widget)
+import qualified Brick.Types            as T
+import qualified Brick.Widgets.Border   as B
+import qualified Brick.Widgets.Center   as C
+import           Brick.Widgets.Core     (hBox, hLimit, str, vBox, vLimit,
+                                         viewport)
+import           Lib                    (getOutput)
+
+data Name = VP1
+          | InputField
+          deriving (Ord, Show, Eq)
+
+data State = State {
+  _output       :: String,
+  _errorMessage :: Maybe String,
+  _input        :: DT.Text,
+  _search       :: DT.Text
+  }
+
+makeLenses ''State
+
+drawUi :: Form State e Name -> [Widget Name]
+drawUi f = [ui]
+    where
+        ui = vBox [ pair, B.hBorder, form ]
+        form = renderForm f
+        state = formState f
+        -- input = vBox [str "input" ]
+        pair = hBox [ viewport VP1 Vertical $
+                      vBox [str $ _output state]
+                    ]
+
+vp1Scroll :: M.ViewportScroll Name
+vp1Scroll = M.viewportScroll VP1
+
+mkForm :: State -> Form State e Name
+mkForm = newForm [ editTextField input InputField (Just 1) ]
+
+appEvent :: (Form State e Name) -> T.BrickEvent Name e -> T.EventM Name (T.Next (Form State e Name))
+appEvent s (T.VtyEvent (V.EvKey V.KDown []))  = M.vScrollBy vp1Scroll 1 >> M.continue s
+appEvent s (T.VtyEvent (V.EvKey V.KUp []))    = M.vScrollBy vp1Scroll (-1) >> M.continue s
+appEvent s (T.VtyEvent (V.EvKey V.KPageDown []))    = M.vScrollPage vp1Scroll T.Down >> M.continue s
+appEvent s (T.VtyEvent (V.EvKey V.KPageUp []))    = M.vScrollPage vp1Scroll T.Up >> M.continue s
+appEvent s (T.VtyEvent (V.EvKey V.KHome []))    = M.vScrollToBeginning vp1Scroll >> M.continue s
+appEvent s (T.VtyEvent (V.EvKey V.KEnd []))    = M.vScrollToEnd vp1Scroll >> M.continue s
+appEvent s (T.VtyEvent (V.EvKey V.KEsc [])) = M.halt s
+appEvent f (T.VtyEvent (V.EvKey V.KEnter [])) =
+  do
+    out <- liftIO $ getOutput $ DT.unpack text
+    M.continue $ mkForm $ state & output .~ out
+  where
+    state = formState f
+    text = state^.input
+
+
+appEvent s ev = do
+  s' <- handleFormEvent ev s
+  M.continue s'
+
+app :: M.App (Form State e Name) e Name
+app =
+    M.App { M.appDraw = drawUi
+          , M.appStartEvent = return
+          , M.appHandleEvent = appEvent
+          , M.appAttrMap = const $ attrMap V.defAttr []
+          , M.appChooseCursor = M.neverShowCursor
+          }
+
+
+run :: IO()
+run = do
+  output <- getOutput "."
+  let initialState = State { _input = ".", _output = output, _errorMessage = Nothing, _search = "" }
+      f = mkForm initialState
+  void $ M.defaultMain app f
