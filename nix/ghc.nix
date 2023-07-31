d860751a42805c973e8cbae5b956015997595c28
Claudio Bley <claudio.bley@gmail.com>
2023-07-31 22:35:28 +0200

    Add `withHoogle` argument

--- nix/ghc.nix
+++ nix/ghc.nix
@@ -1,6 +1,11 @@
-{ pkgs ? import <nixpkgs> { }, ghcVersion }:
+{ pkgs ? import <nixpkgs> { }, ghcVersion, withHoogle ? false }:
 
-pkgs.pkgs.haskell.packages."ghc${builtins.replaceStrings  ["."] [""] ghcVersion}".ghcWithPackages (hs: with hs; [
+let
+  nixGhcVersion = builtins.replaceStrings [ "." ] [ "" ] ghcVersion;
+  ghc = pkgs.pkgs.haskell.packages."ghc${nixGhcVersion}";
+  withPackages = if withHoogle then ghc.ghcWithHoogle else ghc.ghcWithPackages;
+in
+withPackages (hs: with hs; [
   brick
   hinotify
   hspec
