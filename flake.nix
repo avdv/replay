03e685ba5afe2f09ce8450946321ff649a6c8b48
avdv <avdv@users.noreply.github.com>
2025-01-06 09:51:16 +0000

    Update replay.deps hash

--- flake.nix
+++ flake.nix
@@ -100,7 +100,7 @@
                 # make all directories writable
                 find $bazelOut/external/ -type d -exec chmod --changes +w '{}' ';'
               '';
-              sha256 = "sha256-2xUQOAXMgARKPCakWMJy1mcLPqml9ybfz8AmWxe0ZBM=";
+              sha256 = "sha256-1COsheNCWMuh2iN5qtu/YS1hjl/q7MB+DqPGZ2c+9qw=";
             };
 
             buildAttrs = {
