1947760c42a3d05b182ae96df1659b9321ed3f29
avdv <3471749+avdv@users.noreply.github.com>
2025-02-02 01:52:47 +0000

    Update `fetchAttrs.sha256` hash

--- flake.nix
+++ flake.nix
@@ -100,7 +100,7 @@
                 # make all directories writable
                 find $bazelOut/external/ -type d -exec chmod --changes +w '{}' ';'
               '';
-              sha256 = "sha256-lmj8O0rBSgqkLHD4yrcEjb6vNNDaZZtvIpVB06UzB98=";
+              sha256 = "sha256-ZCeHFl9S1DAdsBgcs9VPk8cc41Io+aQb/lwv2+5W+lI=";
             };
 
             buildAttrs = {
