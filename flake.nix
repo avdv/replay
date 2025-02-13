b0a352ee10635c8b177f26c8b696fcbed0736d45
Claudio Bley <claudio.bley@gmail.com>
2025-02-13 22:33:33 +0100

    Remove local_config_xcode external repository

--- flake.nix
+++ flake.nix
@@ -110,6 +110,7 @@
                 rm -rf "$bazelOut/external/"*[~+]{local_config_cc_toolchains,local_config_cc_toolchains.marker}
                 rm -rf "$bazelOut/external/"*[~+]{local_config_sh,local_config_sh.marker}
                 rm -rf "$bazelOut/external/"*[~+]{local_jdk,local_jdk.marker}
+                rm -rf "$bazelOut/external/"*[~+]{local_config_xcode,local_config_xcode.marker}
               '';
               installPhase = ''
                 install -D -t $out/bin bazel-bin/replay
